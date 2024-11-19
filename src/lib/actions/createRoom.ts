b9cf168bcd0961a9ba672f0861ca777d0a6f645a
Subham Mani <manisubham09@gmail.com>
2024-11-19 23:43:37 +0530

    add error handling for room creation and return structured response

--- src/lib/actions/createRoom.ts
+++ src/lib/actions/createRoom.ts
@@ -7,7 +7,12 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/config";
 export async function createRoom() {
   const session = await getServerSession(authOptions);
 
-  if (!session || !session.user) return null;
+  if (!session || !session.user)
+    return {
+      data: null,
+      error: "Unauthenticated",
+      success: false,
+    };
 
   const currentUser = await prisma.user.findUnique({
     where: {
@@ -15,18 +20,38 @@ export async function createRoom() {
     },
   });
 
-  if (!currentUser) return null;
+  if (!currentUser)
+    return {
+      data: null,
+      error: "User not found",
+      success: false,
+    };
+
+  // check if room already exists for user
+
+  const isExistingRoom = await prisma.room.findFirst({
+    where: {
+      owner_id: currentUser.id,
+    },
+  });
+
+  if (isExistingRoom)
+    return {
+      data: null,
+      error: "A room already exists. Please end it first",
+      success: false,
+    };
 
   const room = await prisma.room.create({
     data: {
       roomCode: Math.floor(Math.random() * 1000000).toString(),
-      owner: {
-        connect: {
-          id: currentUser.id,
-        },
-      },
+      owner_id: currentUser.id,
     },
   });
 
-  return room.roomCode;
+  return {
+    data: room.roomCode,
+    success: true,
+    error: null,
+  };
 }

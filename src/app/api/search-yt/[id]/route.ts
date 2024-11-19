a0cb106be6568e92798b362dba870d6cb2878e7d
Subham Mani <manisubham09@gmail.com>
2024-11-19 23:44:17 +0530

    add API route to fetch song details from YouTube Music

---
+++ src/app/api/search-yt/[id]/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import YTMusic from "ytmusic-api";
+
+export async function GET(req: NextRequest) {
+  try {
+    const url = new URL(req.url);
+    const musicId = url.pathname.split("/").pop();
+
+    if (!musicId) throw new Error("Music ID is required");
+
+    const ytmusic = new YTMusic();
+    await ytmusic.initialize();
+
+    const response = await ytmusic.getSong(musicId);
+
+    return NextResponse.json({
+      success: true,
+      data: response,
+      error: null,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      data: error,
+      error: error instanceof Error ? error.message : "Something went wrong",
+    });
+  }
+}

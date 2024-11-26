092f717ec09875fcd19bf66e4ed90cc6dbe0f987
Subham Mani <manisubham09@gmail.com>
2024-11-26 21:24:07 +0530

    refactor MusicDisplay component layout and enhance styling

--- src/components/MusicDisplay.tsx
+++ src/components/MusicDisplay.tsx
@@ -1,10 +1,11 @@
 import MusicSearch from "./MusicSearch";
 
-const MusicDisplay = ({roomCode}: {roomCode: string}) => {
+const MusicDisplay = ({ roomCode }: { roomCode: string }) => {
   return (
-    <div className="border py-6 px-6 rounded-md shadow-sm bg-white">
-      <MusicSearch roomCode={roomCode}/>
-      <h1 className="font-bold text-xl text-start my-6">Currently Playing</h1>
+    <div className="rounded-md h-full pt-6 px-6 bg-white">
+      <h1 className="font-bold text-2xl text-start pb-4">Currently Playing</h1>
+
+      <MusicSearch roomCode={roomCode} />
     </div>
   );
 };

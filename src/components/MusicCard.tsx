1044663a61048161eaa37af9cf1bff357a9e5a91
Subham Mani <manisubham09@gmail.com>
2024-11-26 21:23:41 +0530

    refactor MusicCard component layout and improve accessibility

--- src/components/MusicCard.tsx
+++ src/components/MusicCard.tsx
@@ -11,6 +11,7 @@ export const MusicCard = ({ musicId }: { musicId: string }) => {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
+    // check if music already exists in state
     if (!musicId || music) return;
     async function getMusicInfo() {
       console.log("getting music info: ", musicId);
@@ -25,50 +26,56 @@ export const MusicCard = ({ musicId }: { musicId: string }) => {
   return (
     <li
       key={music.videoId}
-      className="px-2 py-2 my-2 mr-2 rounded-md transition-all duration-200 ease-in-out"
+      className="flex justify-between items-start gap-4 px-2 py-2 my-2 mr-2 rounded-md transition-all duration-200 ease-in-out"
     >
-      <div className="flex items-start gap-3">
+      <div className="flex flex-1 items-start gap-3">
         <Image
           src={music?.thumbnails[1 || 0]?.url ?? ""}
           alt={`${music.name} thumbnail`}
-          className="w-[80px] sm:w-[100px] md:w-[120px] h-[80px] rounded-md object-cover shadow-md"
-          width={120}
+          className="w-[80px] h-[80px] rounded-md object-cover shadow-md"
+          width={80}
           height={80}
         />
         <div className="flex flex-col">
           <span className="line-clamp-2 font-medium text-base text-black">
             {music.name}
           </span>
-          <span className="text-sm text-slate-600">{music.artist.name}</span>
+          <span className="text-sm text-slate-600 line-clamp-1">
+            {music.artist.name}
+          </span>
         </div>
       </div>
-      <div className="mt-2 flex items-center gap-2">
-        <button>
+      <div className="flex flex-col justify-center items-center gap-2">
+        <button className="hover:bg-indigo-100 rounded-full p-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
             fill="none"
-            stroke="#4f46e5"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="size-5 md:size-6"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-4"
           >
-            <polygon points="12 3 4 12 8 12 8 20 16 20 16 12 20 12 12 3" />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m4.5 15.75 7.5-7.5 7.5 7.5"
+            />
           </svg>
         </button>
-        <button>
+        <button className="hover:bg-indigo-100 rounded-full p-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
             fill="none"
-            stroke="#4f46e5"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="size-5 md:size-6 rotate-180"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-4"
           >
-            <polygon points="12 3 4 12 8 12 8 20 16 20 16 12 20 12 12 3" />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m19.5 8.25-7.5 7.5-7.5-7.5"
+            />
           </svg>
         </button>
       </div>

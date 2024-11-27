0ab64d18e46a67be03467305c0a19e4455886939
Subham Mani <manisubham09@gmail.com>
2024-11-27 21:20:01 +0530

    add authentication checks and enhance music queue response with user voting status

--- src/app/api/music-queue/[roomCode]/route.ts
+++ src/app/api/music-queue/[roomCode]/route.ts
@@ -1,5 +1,7 @@
 import prisma from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "@/app/api/auth/[...nextauth]/config";
+import { getServerSession } from "next-auth";
 
 export async function GET(req: NextRequest) {
   try {
@@ -7,6 +9,28 @@ export async function GET(req: NextRequest) {
     const roomCode = url.pathname.split("/").pop();
 
     if (!roomCode) throw new Error("Room Code is required");
+
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user)
+      return {
+        data: null,
+        error: "Unauthenticated",
+        success: false,
+      };
+
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session.user.email!,
+      },
+    });
+
+    if (!currentUser)
+      return {
+        data: null,
+        error: "User not found",
+        success: false,
+      };
 
     const room = await prisma.room.findFirst({
       where: {
@@ -20,13 +44,20 @@ export async function GET(req: NextRequest) {
       where: {
         roomCode: room.roomCode,
       },
-      orderBy: [{ votes: "desc" }, { createdAt: "asc" }],
+      orderBy: [{ votesCount: "desc" }, { createdAt: "asc" }],
+    });
+
+    const musicQueueWithVotes = musicQueue.map((music) => {
+      return {
+        ...music,
+        isVoted: music.votes.includes(currentUser.id),
+      };
     });
 
     return NextResponse.json({
       success: true,
       error: null,
-      data: musicQueue,
+      data: musicQueueWithVotes,
     });
   } catch (error) {
     return NextResponse.json({

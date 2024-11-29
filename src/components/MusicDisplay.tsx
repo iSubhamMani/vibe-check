1d3138497194b1180328c498997e2ed9599c8535
Subham Mani <manisubham09@gmail.com>
2024-11-29 12:46:26 +0530

    enhance MusicDisplay component to manage music queue and current track state

--- src/components/MusicDisplay.tsx
+++ src/components/MusicDisplay.tsx
@@ -1,11 +1,52 @@
-import MusicSearch from "./MusicSearch";
+"use client";
+
+import { useAppDispatch, useAppSelector } from "@/lib/store/hook";
+import { HoverBorderGradient } from "./ui/hover-border-gradient";
+import { setCurrentMusic } from "@/lib/store/slices/musicQueue";
+import VideoPlayer from "./VideoPlayer";
+import { updateQueueAndCurrentMusic } from "@/lib/actions/updateQueueAndCurrentMusic";
 
 const MusicDisplay = ({ roomCode }: { roomCode: string }) => {
-  return (
-    <div className="rounded-md h-full pt-6 px-6 bg-white">
-      <h1 className="font-bold text-2xl text-start pb-4">Currently Playing</h1>
+  const { queue, currentMusic } = useAppSelector((state) => state.musicQueue);
+  const dispatch = useAppDispatch();
 
-      <MusicSearch roomCode={roomCode} />
+  return (
+    <div>
+      {queue.length >= 0 && currentMusic && (
+        <div className="mb-6">
+          <h1 className="font-bold text-2xl text-start pb-4">
+            Currently Playing
+          </h1>
+          <VideoPlayer roomCode={roomCode} />
+        </div>
+      )}
+
+      {queue.length > 0 && !currentMusic && (
+        <div className="mb-6 flex flex-col items-center">
+          <h1 className="font-bold text-xl text-balance text-center pb-4 bg-clip-text text-transparent bg-gradient-to-b from-indigo-500 to-purple-600">
+            Looks like you haven&apos;t started the party yet
+          </h1>
+          <HoverBorderGradient
+            containerClassName="rounded-full"
+            as="button"
+            onClick={async () => {
+              dispatch(setCurrentMusic(queue[0]));
+              await updateQueueAndCurrentMusic(roomCode, queue[0]);
+            }}
+            className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2"
+          >
+            <span>Start Playing</span>
+          </HoverBorderGradient>
+        </div>
+      )}
+
+      {queue.length === 0 && !currentMusic && (
+        <div className="mb-6">
+          <p className="font-bold text-xl text-center bg-clip-text text-transparent bg-gradient-to-b from-indigo-500 to-purple-600">
+            Queue is empty. Add a music to the queue
+          </p>
+        </div>
+      )}
     </div>
   );
 };
